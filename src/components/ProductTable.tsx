811765fef41c2c074a033394271aa62ac302783a
Duje Vatavuk <duje@vatavuk.com>
2024-02-05 14:41:05 +0100

    implement ProductTable

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -1,8 +1,39 @@
 import React from "react";
-import Product from "../models/Product";
+import Product from "../models/Product.ts";
+import ProductCategoryRow from "./ProductCategoryRow.tsx";
+import ProductRow from "./ProductRow.tsx";
 
 export default function ProductTable({ products }: { products: Product[] }) {
+  const rows: JSX.Element[] = [];
+  let lastCategory: string | null = null;
+
+  products.forEach((product) => { 
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow 
+          category={product.category} 
+          key={product.category}
+        />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name}
+      />
+    );
+    lastCategory = product.category;
+  });
+
   return (
-    <div>Product table</div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
   );
 }
